40380b6aa95a1e78b5c9b6e0608ced7f1ecd916b
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-04-29 22:07:20 +0200

    objects added to the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
-var pokemonList = [];
+var pokemonList = [{
+    name: Pikachu,
+    height: 0.4,
+    types: ['electric'],
+    Weight: 6
+  },
+  {
+    name: Bulbasaur,
+    height: 0.7,
+    types: ['grass', 'poison'],
+    Weight: 6.9
+  },
+  {
+    name: Charizard,
+    height: 1.7,
+    types: ['fire', 'flying'],
+    Weight: 90.5
+  },
+  {
+    name: Venusaur,
+    height: 2,
+    types: ['grass', 'poison'],
+    Weight: 100
+  },
+  {
+    name: Dragonite,
+    height: 2.2,
+    types: ['dragon', 'flying'],
+    Weight: 210
+  }
+];
