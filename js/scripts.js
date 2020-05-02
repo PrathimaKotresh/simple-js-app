19114ea51f8c7edbf55a31085365696fd4d174c0
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-05-02 11:28:58 +0200

    fixed pokemon names to make it a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 var pokemonList = [{
-    name: Pikachu,
+    name: 'Pikachu',
     height: 0.4,
     types: ['electric'],
     Weight: 6
   },
   {
-    name: Bulbasaur,
+    name: 'Bulbasaur',
     height: 0.7,
     types: ['grass', 'poison'],
     Weight: 6.9
   },
   {
-    name: Charizard,
+    name: 'Charizard',
     height: 1.7,
     types: ['fire', 'flying'],
     Weight: 90.5
   },
   {
-    name: Venusaur,
+    name: 'Venusaur',
     height: 2,
     types: ['grass', 'poison'],
     Weight: 100
   },
   {
-    name: Dragonite,
+    name: 'Dragonite',
     height: 2.2,
     types: ['dragon', 'flying'],
     Weight: 210
