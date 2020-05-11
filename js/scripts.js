7933906ff4f4883972352b9c5efcb50c9cecf4af
Prathima Kotresh <prathima.kotresh@gmail.com>
2020-05-11 21:09:21 +0200

    wrap your pokemonList array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
-var pokemonList = [{
-    name: 'Pikachu',
-    height: 0.4,
-    types: ['electric'],
-    Weight: 6
-  },
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison'],
-    Weight: 6.9
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying'],
-    Weight: 90.5
-  },
-  {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison'],
-    Weight: 100
-  },
-  {
-    name: 'Dragonite',
-    height: 2.2,
-    types: ['dragon', 'flying'],
-    Weight: 210
-  }
-];
+var pokemonRepository = (function () {
+  var pokemonList = [{
+      name: 'Pikachu',
+      height: 0.4,
+      types: ['electric'],
+      Weight: 6
+    },
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison'],
+      Weight: 6.9
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying'],
+      Weight: 90.5
+    },
+    {
+      name: 'Venusaur',
+      height: 2,
+      types: ['grass', 'poison'],
+      Weight: 100
+    },
+    {
+      name: 'Dragonite',
+      height: 2.2,
+      types: ['dragon', 'flying'],
+      Weight: 210
+    }
+  ];
+})();
 
 pokemonList.forEach(function(pokemon){
   document.write(pokemon.name + ' (height: ' + pokemon.height + ')');
